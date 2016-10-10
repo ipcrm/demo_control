1bb075eb9b9d506bd875d8990bdf1af266d21eb9
ipcrm <mcadorette@gmail.com>
2016-10-10 18:11:23 -0400

    fixing reg keys

--- site/profile/manifests/base/security/ie_esc.pp
+++ site/profile/manifests/base/security/ie_esc.pp
@@ -4,20 +4,20 @@ class profile::base::security::ie_esc (
 
   if $::kernel == 'Windows' and $ie_esc_enabled == false {
 
-    registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\IsInstalled':
+    registry_value { '32:HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\IsInstalled':
       type => 'dword',
       data => '00000000',
     }
 
     registry_value {
-      'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+      '32:HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
       type   => 'dword',
       data   => '00000000',
       notify => Exec['gpupdate'],
     }
 
     registry_value {
-      'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+      '32:HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
       type   => 'dword',
       data   => '00000000',
       notify => Exec['gpupdate'],
