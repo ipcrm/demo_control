b34402143664d61b49ae7b6703e7aed8c3293fd4
ipcrm <mcadorette@gmail.com>
2016-10-10 18:08:09 -0400

    updating windows motd logic

--- site/profile/manifests/base/motd.pp
+++ site/profile/manifests/base/motd.pp
@@ -2,8 +2,27 @@ class profile::base::motd (
   String $message = 'Welcome to the demo enviornment network!/n',
 ){
 
-  class { 'motd':
-      content => $message,
-  }
+  case $::facts['os']['name'] {
+    'Windows': {
+
+      registry_value { '32:HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system\legalnoticecaption':
+        ensure => present,
+        type   => string,
+        data   => 'Message of the day',
+      }
+
+      registry_value { '32:HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system\legalnoticetext':
+        ensure => present,
+        type   => string,
+        data   => $message,
+      }
+
+    }
+
+    default: {
+      class { 'motd':
+          content => $message,
+      }
+    }
 
 }
