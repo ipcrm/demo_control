1eb24b96197fbec2941d68d7737d027562bd9ef4
ipcrm <mcadorette@gmail.com>
2016-10-13 08:38:08 -0400

    adding refresh classes

--- site/profile/manifests/master/node_groups.pp
+++ site/profile/manifests/master/node_groups.pp
@@ -3,10 +3,17 @@ class profile::master::node_groups {
   package { 'puppetclassify':
     ensure   => present,
     provider => puppet_gem,
+    notify   => Exec['refresh_classes'],
   }
 
   Node_group {
     require => Package['puppetclassify'],
+  }
+
+  exec{'refresh_classes':
+    path        => $::path,
+    command     => "/etc/puppetlabs/code/${::environment}/production/scripts/refresh_classes.sh",
+    refreshonly => true,
   }
 
   node_group { 'role::master':
