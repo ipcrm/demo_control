d762d24dd1c32cf5484fdbd275103f0448575c6e
ipcrm <mcadorette@gmail.com>
2016-10-13 16:01:28 -0400

    fixing paths

--- site/profile/manifests/master/node_groups.pp
+++ site/profile/manifests/master/node_groups.pp
@@ -12,7 +12,7 @@ class profile::master::node_groups {
 
   exec{'refresh_classes':
     path        => $::path,
-    command     => "sh -x /etc/puppetlabs/code/${::environment}/scripts/refresh_classes.sh",
+    command     => "/etc/puppetlabs/code/environments/${::environment}/scripts/refresh_classes.sh",
     refreshonly => true,
   }
 
