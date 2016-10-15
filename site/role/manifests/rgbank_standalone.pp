c6cc169b815a9f394971e0a9fa53d7c945ee314e
ipcrm <mcadorette@gmail.com>
2016-10-15 15:40:57 -0400

    adding wordpress to rgbank_standalone role

--- site/role/manifests/rgbank_standalone.pp
+++ site/role/manifests/rgbank_standalone.pp
@@ -3,6 +3,7 @@ class role::rgbank_standalone {
   include ::profile::base
   include ::profile::database::mysql
   include ::profile::webserver::apache
+  include ::profile::apps::wordpress
   include ::profile::apps::rgbank
 
 }
