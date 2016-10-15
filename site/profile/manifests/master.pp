c3776c38055bd2b65983829cef2d4537cf54c253
ipcrm <mcadorette@gmail.com>
2016-10-15 11:04:23 -0400

    adding repos to master

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -3,5 +3,6 @@ class profile::master {
   contain ::profile::master::puppetserver
   contain ::profile::master::node_groups
   contain ::profile::master::fileserver
+  contain ::profile::master::repos
 
 }
---
+++ site/profile/manifests/master/repos.pp
@@ -0,0 +1,6 @@
+class profile::master::repos {
+
+  include ::pe_repo::platform::windows_x86_64
+
+}
+
