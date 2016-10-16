8e217aca3cc18649f45c4a53da9a195736a888cd
ipcrm <mcadorette@gmail.com>
2016-10-15 22:51:26 -0400

    adding rgbank instance

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,14 +4,15 @@
 File { backup => false }
 
 # Applications managed by App Orchestrator are defined in the site block.
-#site {
-#  rgbank { 'getting-started':
-#    listen_port => 8010,
-#    nodes       => {
-#        Node['appserver1d.pdx.puppetlabs.demo'] => [Rgbank::Db[getting-started]],
-#        Node['appserver1b.pdx.puppetlabs.demo'] => [Rgbank::Web[appserver-01_getting-started]],
-#        Node['appserver1c.pdx.puppetlabs.demo'] => [Rgbank::Web[appserver-02_getting-started]],
-#        Node['appserver1a.pdx.puppetlabs.demo'] => [Rgbank::Load[getting-started]],
-#    },
-#  }
-#}
+site {
+  rgbank { 'getting-started':
+    listen_port => 8010,
+    nodes       =>
+    {
+      Node['db1.demo.lan']  =>[Rgbank::Db[getting-started]],
+      Node['app1.demo.lan'] => [Rgbank::Web[appserver-01_getting-started]],
+      #Node['appserver1c.pdx.puppetlabs.demo'] => [Rgbank::Web[appserver-02_getting-started]],
+      Node['lb1.demo.lan']  => [Rgbank::Load[getting-started]],
+    },
+  }
+}
