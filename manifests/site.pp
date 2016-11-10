1aee5a8748ab2dcd8b71ceb16a860c1130674f2f
ipcrm <mcadorette@gmail.com>
2016-11-09 22:25:06 -0500

    updating

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,16 +15,16 @@ site {
 #    },
 #  }
 
-#  cloudshop { 'split':
-#    administrator => 'Administrator',
-#    dbinstance    => 'MYINSTANCE',
-#    dbuser        => 'CloudShop',
-#    dbpassword    => 'Azure$123',
-#    dbname        => 'AdventureWorks2012',
-#    nodes         => {
-#      Node['example7.demo.lan'] => Cloudshop::App['split'],
-#      Node['example8.demo.lan'] => Cloudshop::Db['split'],
-#    },
-#  }
+  cloudshop { 'split':
+    administrator => 'Administrator',
+    dbinstance    => 'MYINSTANCE',
+    dbuser        => 'CloudShop',
+    dbpassword    => 'Azure$123',
+    dbname        => 'AdventureWorks2012',
+    nodes         => {
+      Node['example7.demo.lan'] => Cloudshop::App['split'],
+      Node['example8.demo.lan'] => Cloudshop::Db['split'],
+    },
+  }
 
 }
