4ef29d0639e07c04c3a23dd9291a9ce8d7bf9582
ipcrm <mcadorette@gmail.com>
2016-11-10 10:41:57 -0500

    Updating apps

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,16 +4,16 @@ File { backup => false }
 # Applications managed by App Orchestrator are defined in the site block.
 site {
 
-#  rgbank { 'getting-started':
-#    listen_port => 8010,
-#    nodes       =>
-#    {
-#      Node['db2.demo.lan']  => [Rgbank::Db[getting-started]],
-#      Node['app2.demo.lan'] => [Rgbank::Web[appserver-01_getting-started]],
-#      Node['app3.demo.lan'] => [Rgbank::Web[appserver-02_getting-started]],
-#      Node['lb2.demo.lan']  => [Rgbank::Load[getting-started]],
-#    },
-#  }
+  rgbank { 'getting-started':
+    listen_port => 8010,
+    nodes       =>
+    {
+      Node['db2.demo.lan']  => [Rgbank::Db[getting-started]],
+      Node['app2.demo.lan'] => [Rgbank::Web[appserver-01_getting-started]],
+      Node['app3.demo.lan'] => [Rgbank::Web[appserver-02_getting-started]],
+      Node['lb2.demo.lan']  => [Rgbank::Load[getting-started]],
+    },
+  }
 
 #  cloudshop { 'split':
 #    dbinstance    => 'MYINSTANCE',
