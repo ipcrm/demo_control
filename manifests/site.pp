cb5b51a336ab61958ad6e26264be196534edd646
ipcrm <mcadorette@gmail.com>
2016-11-10 16:24:19 -0500

    updating apps

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,29 +4,29 @@ File { backup => false }
 # Applications managed by App Orchestrator are defined in the site block.
 site {
 
-  rgbank { 'getting-started':
-    listen_port => 8010,
-    nodes       =>
-    {
-      Node['db2.demo.lan']  => [Rgbank::Db[getting-started]],
-      Node['app2.demo.lan'] => [Rgbank::Web[appserver-01_getting-started]],
-      Node['app3.demo.lan'] => [Rgbank::Web[appserver-02_getting-started]],
-      Node['app4.demo.lan'] => [Rgbank::Web[appserver-03_getting-started]],
-      Node['app5.demo.lan'] => [Rgbank::Web[appserver-04_getting-started]],
-      Node['lb2.demo.lan']  => [Rgbank::Load[getting-started]],
-    },
-  }
-
-  cloudshop { 'split':
-    dbinstance    => 'MYINSTANCE',
-    dbuser        => 'CloudShop',
-    dbpassword    => 'Azure$123',
-    dbname        => 'AdventureWorks2012',
-    nodes         => {
-      Node['example7.demo.lan'] => Cloudshop::App['split'],
-      Node['example8.demo.lan'] => Cloudshop::Db['split'],
-    },
-  }
+#  rgbank { 'getting-started':
+#    listen_port => 8010,
+#    nodes       =>
+#    {
+#      Node['db2.demo.lan']  => [Rgbank::Db[getting-started]],
+#      Node['app2.demo.lan'] => [Rgbank::Web[appserver-01_getting-started]],
+#      Node['app3.demo.lan'] => [Rgbank::Web[appserver-02_getting-started]],
+#      Node['app4.demo.lan'] => [Rgbank::Web[appserver-03_getting-started]],
+#      Node['app5.demo.lan'] => [Rgbank::Web[appserver-04_getting-started]],
+#      Node['lb2.demo.lan']  => [Rgbank::Load[getting-started]],
+#    },
+#  }
+#
+#  cloudshop { 'split':
+#    dbinstance    => 'MYINSTANCE',
+#    dbuser        => 'CloudShop',
+#    dbpassword    => 'Azure$123',
+#    dbname        => 'AdventureWorks2012',
+#    nodes         => {
+#      Node['example7.demo.lan'] => Cloudshop::App['split'],
+#      Node['example8.demo.lan'] => Cloudshop::Db['split'],
+#    },
+#  }
 
 
 }
