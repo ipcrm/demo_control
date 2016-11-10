af9331f43f16f9116c7938ed613c6483592b0971
ipcrm <mcadorette@gmail.com>
2016-11-09 22:48:19 -0500

    Updating to use new broke out iis profile

---
+++ site/profile/manifests/webserver/iis.pp
@@ -0,0 +1,44 @@
+class profile::webserver::iis {
+  $iis_features = [
+      'Web-Server',
+      'Net-Framework-45-ASPNET',
+      'Application-Server',
+      'AS-NET-Framework',
+      'AS-Web-Support',
+      'Web-Mgmt-Tools',
+      'Web-Mgmt-Console',
+      'Web-Scripting-Tools',
+      'Web-WebServer',
+      'Web-App-Dev',
+      'Web-Asp-Net45',
+      'Web-ISAPI-Ext',
+      'Web-ISAPI-Filter',
+      'Web-Net-Ext45',
+      'Web-Common-Http',
+      'Web-Default-Doc',
+      'Web-Dir-Browsing',
+      'Web-Http-Errors',
+      'Web-Http-Redirect',
+      'Web-Static-Content',
+      'Web-Health',
+      'Web-Http-Logging',
+      'Web-Log-Libraries',
+      'Web-Request-Monitor',
+      'Web-Stat-Compression',
+      'Web-Dyn-Compression',
+      'Web-Security',
+      'Web-Basic-Auth',
+      'Web-Cert-Auth',
+      'Web-Client-Auth',
+      'Web-Digest-Auth',
+      'Web-Filtering',
+      'Web-IP-Security',
+      'Web-Url-Auth',
+      'Web-Windows-Auth',
+    ]
+
+  windowsfeature { $iis_features:
+    ensure => present,
+  }
+
+}
--- site/role/manifests/win_sqlweb_demo.pp
+++ site/role/manifests/win_sqlweb_demo.pp
@@ -2,6 +2,7 @@ class role::win_sqlweb_demo {
 
   include ::profile::base
   include ::profile::database::sqlserver
+  include ::profile::webserver::iis
   include ::profile::apps::cloudshop
 
 }
