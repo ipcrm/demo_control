bc56d4613b47b6f72c96d876b9774085314af6d6
ipcrm <mcadorette@gmail.com>
2016-11-09 20:57:24 -0500

    removing apps

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,27 +3,28 @@ File { backup => false }
 
 # Applications managed by App Orchestrator are defined in the site block.
 site {
-  rgbank { 'getting-started':
-    listen_port => 8010,
-    nodes       =>
-    {
-      Node['db2.demo.lan']  => [Rgbank::Db[getting-started]],
-      Node['app2.demo.lan'] => [Rgbank::Web[appserver-01_getting-started]],
-      Node['app3.demo.lan'] => [Rgbank::Web[appserver-02_getting-started]],
-      Node['lb2.demo.lan']  => [Rgbank::Load[getting-started]],
-    },
-  }
 
-  cloudshop { 'split':
-    administrator => 'Administrator',
-    dbinstance    => 'MYINSTANCE',
-    dbuser        => 'CloudShop',
-    dbpassword    => 'Azure$123',
-    dbname        => 'AdventureWorks2012',
-    nodes         => {
-      Node['example7.demo.lan'] => Cloudshop::App['split'],
-      Node['example8.demo.lan'] => Cloudshop::Db['split'],
-    },
-  }
+#  rgbank { 'getting-started':
+#    listen_port => 8010,
+#    nodes       =>
+#    {
+#      Node['db2.demo.lan']  => [Rgbank::Db[getting-started]],
+#      Node['app2.demo.lan'] => [Rgbank::Web[appserver-01_getting-started]],
+#      Node['app3.demo.lan'] => [Rgbank::Web[appserver-02_getting-started]],
+#      Node['lb2.demo.lan']  => [Rgbank::Load[getting-started]],
+#    },
+#  }
+
+#  cloudshop { 'split':
+#    administrator => 'Administrator',
+#    dbinstance    => 'MYINSTANCE',
+#    dbuser        => 'CloudShop',
+#    dbpassword    => 'Azure$123',
+#    dbname        => 'AdventureWorks2012',
+#    nodes         => {
+#      Node['example7.demo.lan'] => Cloudshop::App['split'],
+#      Node['example8.demo.lan'] => Cloudshop::Db['split'],
+#    },
+#  }
 
 }
