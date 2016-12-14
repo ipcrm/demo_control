2d40e1e325149bf5c156a72146fa4ede46460ced
ipcrm <mcadorette@gmail.com>
2016-12-14 16:08:38 -0500

    defaulting services

--- site/profile/manifests/base/services.pp
+++ site/profile/manifests/base/services.pp
@@ -1,5 +1,5 @@
 class profile::base::services {
-  $services = hiera_hash('services')
+  $services = hiera_hash('services',{})
   $_os = downcase($::facts['os']['name'])
   $_os_major = $::facts['os']['release']['major']
   $_os_string = "${_os}${_os_major}"
