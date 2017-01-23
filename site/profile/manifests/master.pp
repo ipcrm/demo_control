947ca2204ae180d43a6b1ea415edf381d2b2124e
ipcrm <mcadorette@gmail.com>
2017-01-22 20:40:42 -0500

    test

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -1,5 +1,9 @@
 class profile::master {
 
+  notify{'Test Message':
+    message => hiera('testvalue'),
+  }
+
   contain ::profile::master::puppetserver
   contain ::profile::master::node_groups
   contain ::profile::master::fileserver
