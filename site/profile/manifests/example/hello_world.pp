a8fe49d7081460bde3ab0bf8b1a9bd851d55d992
ipcrm <mcadorette@gmail.com>
2017-01-26 07:42:50 -0500

    tidying

--- site/profile/manifests/example/hello_world.pp
+++ site/profile/manifests/example/hello_world.pp
@@ -2,12 +2,13 @@ class profile::example::hello_world (
   $war_files       = ['helloworld.war'],
   $war_source      = 'http://master.demo.lan/artifacts',
   $tomcat_source   = 'https://www.apache.org/dist/tomcat/tomcat-8/v8.5.11/bin/apache-tomcat-8.5.11.tar.gz',
-  $manage_firewall = true
+  $manage_firewall = true,
+  $java_opts       = ['-server',
+                      '-Djava.net.preferIPv4Stack=true',
+                      '-Djava.net.preferIPv4Addresses',
+                      '-Djava.security.egd=file:/dev/./urandom',
+  ]
 ){
-
-  package {'haveged':
-    ensure  => present,
-  }
 
   java::oracle { 'jdk8' :
     ensure  => 'present',
@@ -19,24 +20,14 @@ class profile::example::hello_world (
     install_from    => 'archive',
     version         => '8.0.15',
     catalina_home   => '/opt/tomcat',
-    java_opts       => [
-      '-server',
-      '-Djava.net.preferIPv4Stack=true',
-      '-Djava.net.preferIPv4Addresses',
-      '-Djava.security.egd=file:/dev/./urandom',
-    ],
+    java_opts       => $java_opts,
     manage_firewall => $manage_firewall,
   }
 
   tomcat::instance { 'instance2':
     version             => '8.0.18',
     catalina_home       => '/opt/tomcat2',
-    java_opts           => [
-      '-server',
-      '-Djava.net.preferIPv4Stack=true',
-      '-Djava.net.preferIPv4Addresses',
-      '-Djava.security.egd=file:/dev/./urandom',
-    ],
+    java_opts           => $java_opts,
     server_control_port => 8006,
     http_port           => 8081,
     ajp_port            => 8109,
@@ -44,7 +35,6 @@ class profile::example::hello_world (
   }
 
   $war_files.each |$war_file| {
-
 
     file {"/opt/tomcat/webapps/${war_file}":
       ensure => present,
