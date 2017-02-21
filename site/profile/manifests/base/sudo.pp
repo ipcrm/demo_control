3fadea25e33fe8f31674de2713fb591a85ec3b07
ipcrm <mcadorette@gmail.com>
2017-02-21 16:56:12 -0500

    test

--- site/profile/manifests/base/sudo.pp
+++ site/profile/manifests/base/sudo.pp
@@ -1,10 +1,15 @@
 class profile::base::sudo {
 
-  include ::sudo
+  class {'::sudo': }
 
   sudo::conf { 'admins':
     priority => 10,
     content  => '%admins ALL=(ALL) NOPASSWD: ALL',
+  }
+
+  sudo::conf { 'centos':
+    priority => 10,
+    content  => 'centos ALL=(ALL) NOPASSWD:ALL',
   }
 
 }
