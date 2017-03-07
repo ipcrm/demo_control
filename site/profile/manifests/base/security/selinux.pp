4e3b81c124d457d32c86fe0cfd9bccd46083c204
ipcrm <mcadorette@gmail.com>
2017-03-06 22:00:29 -0500

    dont configure selinux in docker

--- site/profile/manifests/base/security/selinux.pp
+++ site/profile/manifests/base/security/selinux.pp
@@ -2,7 +2,7 @@ class profile::base::security::selinux (
   $selinux_mode = 'disabled',
 ){
 
-  if $::facts['os']['family'] == 'RedHat' {
+  if $::facts['os']['family'] == 'RedHat' and $::virtual != 'docker' {
     class { '::selinux':
       mode   => $selinux_mode,
       notify => Reboot['selinux'],
