5451f460ec9b276696ab7ec587978dba7d5b33be
ipcrm <mcadorette@gmail.com>
2017-03-16 00:20:54 -0400

    enable apps

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,6 +15,27 @@ site {
       Node['flask-1.demo.lan'] => [Flask_app::Webhead['dev']],
     },
   }
+
+  $environment = get_compiler_environment()
+
+  # Dynamic application declarations
+  # from JSON
+  $envs = loadyaml("/etc/puppetlabs/code/environments/${environment}/applications.yaml")
+  $applications = pick_default($envs[$environment], {})
+
+  $applications.each |String $type, $instances| {
+    $instances.each |String $title, $params| {
+      $parsed_parameters = $params.make_application_parameters($title)
+
+      # Because Puppet code expects typed parameters, not just strings representing
+      # types, an appropriately transformed version of the $params variable will be
+      # used. The resolve_resources() method comes from the tse/to_resource module.
+      Resource[$type] { $title:
+        * => $parsed_parameters.resolve_resources,
+      }
+    }
+  }
+
 }
 
 ##  cloudshop { 'solo':
@@ -38,26 +59,6 @@ site {
 ##    },
 ##  }
 #
-#
-#  $environment = get_compiler_environment()
-#
-#  # Dynamic application declarations
-#  # from JSON
-#  $envs = loadyaml("/etc/puppetlabs/code/environments/${environment}/applications.yaml")
-#  $applications = pick_default($envs[$environment], {})
-#
-#  $applications.each |String $type, $instances| {
-#    $instances.each |String $title, $params| {
-#      $parsed_parameters = $params.make_application_parameters($title)
-#
-#      # Because Puppet code expects typed parameters, not just strings representing
-#      # types, an appropriately transformed version of the $params variable will be
-#      # used. The resolve_resources() method comes from the tse/to_resource module.
-#      Resource[$type] { $title:
-#        * => $parsed_parameters.resolve_resources,
-#      }
-#    }
-#  }
 #}
 
 if pick($::noop_disable,false) == false {
