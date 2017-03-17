8f56bdc1932b4586af44acdc5162cd657112a6f8
ipcrm <mcadorette@gmail.com>
2017-03-17 17:27:13 -0400

    Updating jenkins to use oracle jdk

--- site/profile/manifests/apps/jenkins.pp
+++ site/profile/manifests/apps/jenkins.pp
@@ -2,9 +2,16 @@ class profile::apps::jenkins (
   $version = '2.32.3',
 ){
 
+  java::oracle { 'jdk8' :
+    ensure  => 'present',
+    version => '8',
+    java_se => 'jdk',
+  }
+
   class { '::jenkins':
     version            => $version,
     configure_firewall => true,
+    install_java       => false,
   }
 
 }
