7e3bf3e5cd90ccbb1ed5ae659a701e57616baa23
ipcrm <mcadorette@gmail.com>
2017-06-20 08:29:21 -0400

    more elk tuning

--- site/profile/manifests/apps/elasticsearch.pp
+++ site/profile/manifests/apps/elasticsearch.pp
@@ -1,15 +1,21 @@
 class profile::apps::elasticsearch {
+  $config_hash = {
+    'ES_HEAP_SIZE' => '3g',
+  }
+
+
   class { '::elasticsearch':
-    java_install => true,
-    manage_repo  => true,
-    repo_version => '2.x',
-    config       => {
+    java_install  => true,
+    manage_repo   => true,
+    repo_version  => '2.x',
+    config        => {
       'network.host' => '0.0.0.0',
     },
-    jvm_options  => [
+    jvm_options   => [
       '-Xms3g',
       '-Xmx3g',
     ],
+    init_defaults => $config_hash,
   }
 
   elasticsearch::instance { 'es-01': }
