70e2d73c1b4531cd26e8fcd625b927a7eacafd2f
ipcrm <mcadorette@gmail.com>
2017-07-07 18:52:34 -0400

    adding more examples

---
+++ site/profile/manifests/cisco/bgp.pp
@@ -0,0 +1,21 @@
+class profile::cisco::bgp {
+  cisco_command_config { 'features':
+    command => "
+      feature bgp
+    "
+  }
+
+  cisco_bgp { '65001 default':
+    ensure      => 'present',
+    maxas_limit => '8',
+    router_id   => '192.0.2.4',
+    shutdown    => false,
+    require     => Cisco_command_config['features'],
+  }
+
+  cisco_bgp_neighbor { '65001 default 192.0.3.1':
+    ensure    => 'present',
+    remote_as => '65004',
+    require   => Cisco_bgp['65001 default'],
+  }
+}
---
+++ site/profile/manifests/cisco/vxlan.pp
@@ -0,0 +1,16 @@
+class profile::cisco::vxlan {
+  cisco_vxlan_vtep { 'nve1':
+    ensure            => present,
+    description       => 'Configured by puppet',
+    host_reachability => 'evpn',
+    shutdown          => false,
+    source_interface  => 'loopback1',
+  }
+
+  cisco_vxlan_vtep_vni {'nve1 10000':
+    ensure              => present,
+    assoc_vrf           => false,
+    ingress_replication => 'static',
+    peer_list           => ['4.4.4.4', '5.5.5.5'],
+  }
+}
